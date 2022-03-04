8e16c11f00183db52fcc815568d2d65113e77835
Deirael <100673170+Deirael@users.noreply.github.com>
2022-03-04 16:52:05 +0800

    Lecture 1 Assignment

---
+++ Lecture1/Assignments/assignment_lec1_c2.c.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+
+	int difference, value1, value2;
+
+    value1 = 15;
+    value2 = 87;
+	difference = value2 - value1;
+
+	printf("The difference when 15 is subtracted from 87 is %d.\n", difference);
+
+	return 0;
+}
