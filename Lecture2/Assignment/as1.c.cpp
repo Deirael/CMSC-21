7330e69364e0a0b5936e3afd5e10ca24f36a2ef2
Deirael <100673170+Deirael@users.noreply.github.com>
2022-03-14 15:38:14 +0800

    Lecture 2 Assignment

---
+++ Lecture2/Assignment/as1.c.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, answer = 0;
+
+    printf("Enter a 2-digit number: ");
+    scanf("%d",&number);
+
+    while(number > 0)
+    {
+        int lastnum = number % 10;
+
+        answer = answer * 10 + lastnum;
+
+        number = number / 10;
+    }
+
+    printf("The reversed number is %d\n", answer);
+
+    return 0;
+}
