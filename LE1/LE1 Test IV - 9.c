54ef0bf1201a09f74a95c7c4e4637f76fd112956
Deirael <100673170+Deirael@users.noreply.github.com>
2022-04-22 15:12:48 +0800

    First Long Exam

---
+++ LE1/LE1 Test IV - 9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(){
+    int row, column = 0;
+    int size = 0 ;
+    char cont = 'y';
+
+    while(cont == ‘y’){
+        printf("Enter square size:");
+        scanf("%d", &size);
+
+    for( row = 0 ;row < size ; row++){
+        for(column = 0 ; column < size;column++){
+            if (i = 0 || j = 0|| i = size|| j = size){
+                printf("*");
+        }
+        else{
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+
+    printf("Print another square? Enter y or n: ");
+    scanf (“%c”, &cont);
+
+    if (cont == 'n'){
+        printf(“You have exited the program, thank you for using!”);
+    }
+    else if (cont != ‘y’ && cont != ‘n’){
+        printf("Not a valid choice. \n");
+
+        printf("Print another square? Enter y/n: ");
+        scanf (“%c”, &cont);
+    }
+    }
+    return 0;
+}
